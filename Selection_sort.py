f12b083bebefa1d508136bdb1087c204792b73f9
Atul Kumar <rajputatul2002@gmail.com>
2025-04-21 23:39:34 +0530

    Create Selection_sort.py

---
+++ Selection_sort.py
@@ -0,0 +1,14 @@
+# Selection Sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    print(arr)
+
+
+arr = [1,4,2,3,6,7,5]
+selection_sort(arr)
